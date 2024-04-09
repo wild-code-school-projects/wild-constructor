95b41174dcdb78b23bc91241fbdd9bc4c180ffca
SuperCoolNinja <sbpro570@gmail.com>
2024-04-09 15:22:21 +0200

    init

---
+++ Building.cs
@@ -0,0 +1,24 @@
+﻿namespace Constructor;
+
+internal class Building
+{
+    public Building(int etages, int taille)
+    {
+        _etages = etages;
+        _taille = taille;
+    }
+    public Building(int etages)
+    {
+        _etages = etages;
+        _taille *= etages; // 3 * etages
+    }
+
+    public int GetFloorMaxSize() => _taille / _etages;
+    public int GetFloorCount() => _etages;
+    public int GetSize() => _taille;
+
+
+    private const int _DefaultTaille = 3;
+    private int _etages;
+    private int _taille = _DefaultTaille;
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var building = new Building(5);
+
+            Console.WriteLine($"Taille :{building.GetSize()}m");
+            Console.WriteLine($"Taille maximum de un étage :{building.GetFloorMaxSize()}m");
+            Console.WriteLine($"Nombre d'étages :{building.GetFloorCount()}");
+        }
+    }
+}
